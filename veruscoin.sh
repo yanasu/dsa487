25fca18101d000bcddc340b7e924772a47e69f07
yanasu <86538990+yanasu@users.noreply.github.com>
2021-06-26 20:53:32 -0300

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,5 @@
+# veruscoin
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c estrato+tcp://ue.luckpool.net:3956#xnsub -u RBRfzNeRcyPWeRQYVtDAV15qtraKCXBmeE.01-2606 -px --cpu 8
